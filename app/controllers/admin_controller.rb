058561ebe96e7936025e73eeaa2dd23fe8b22f1c
David Backeus <duztdruid@gmail.com>
2016-06-06 21:33:29 +0200

    Order in admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,11 +4,11 @@ class AdminController < ApplicationController
   http_basic_authenticate_with name: "lol", password: ""
 
   def registrations
-    @participants = Participant.all.includes(:registration)
+    @participants = Participant.all.order(:created_at).includes(:registration)
   end
 
   def transport
-    @registrations = Registration.where(transport: true).where.not(departure_at: nil).includes(:participants)
-    @pending_registrations = Registration.where(transport: true).where(departure_at: nil).includes(:participants)
+    @registrations = Registration.order(:created_at).where(transport: true).where.not(departure_at: nil).includes(:participants)
+    @pending_registrations = Registration.order(:created_at).where(transport: true).where(departure_at: nil).includes(:participants)
   end
 end
